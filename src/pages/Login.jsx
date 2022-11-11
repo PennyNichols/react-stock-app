6d5abe8016292102e0c27775cfe8ade4e4580b11
pnichols789 <pnichols789@gmail.com>
2022-11-10 19:29:37 -0600

    -

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ErrorMessage, Formik, Form, Field } from 'formik'
+import Img from '../assets/result.svg'
+import {AuthContext} from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
+
 
 const Login = () => {
+    const { signIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+
   return (
     <div>Login</div>
   )
